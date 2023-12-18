08c926cc758dc6d1c634e2386f825be1015a8c6a
tauha <khalilgaming8899@gmail.com>
2023-12-18 21:13:44 +0500

    syn scan , xmas scan and vanilla scan alll are working , single host multiple ports are also working

--- modules/network/port_scanner.py
+++ modules/network/port_scanner.py
@@ -3,7 +3,7 @@
 import os
 import socket
 from threading import Thread
-from scapy.all import ARP, Ether, srp, IP, TCP, sr1
+from scapy.all import ARP, Ether, srp, IP, TCP, sr1,sr,ICMP,send
 import concurrent.futures
 
 # ... (your existing code for the discover_hosts function and other utility functions)
@@ -38,22 +38,49 @@ def discover_hosts(network_prefix, subnet_mask):
     else:
         return "Invalid subnet mask. Please enter a number between 0 and 32.", []
         
-def syn_scan(target_ip, port):
-    response = sr1(IP(dst=target_ip) / TCP(dport=port, flags="S"), timeout=1, verbose=False)
-    if response and response.haslayer(TCP):
-        if response[TCP].flags == 0x12:  # SYN-ACK flag
-            return f"Port {port} is open on {target_ip}"
-    return f"Port {port} is closed on {target_ip}"
 
-def perform_syn_scan(ip_addresses, port):
+
+
+
+def syn_scan(target, port):
+    
+    # Check if target and port are valid
+    if not isinstance(target, str) or not isinstance(port, int):
+        print("Invalid target or port. Target must be a string and port must be an integer.")
+        return
+
+    try:
+        pkt = IP(dst=target) / TCP(dport=port, flags="S")
+        scan = sr1(pkt, timeout=5, verbose=0)
+    except Exception as e:
+        print(f"Error creating or sending packet: {e}")
+        return
+
+    if scan is None:
+        print(f"Port {port}: Filtered")
+    elif scan.haslayer(TCP):
+        if scan.getlayer(TCP).flags == 0x12:  # 0x12 SYN+ACk
+            IP(dst=target) / TCP(dport=port, flags="R")  # Send a RST packet
+            print(f"Port {port}: Open")
+        elif scan.getlayer(TCP).flags == 0x14:
+            print(f"Port {port}: Closed")
+    elif scan.haslayer(ICMP):
+        if int(scan.getlayer(ICMP).type) == 3 and int(scan.getlayer(ICMP).code in [1, 2, 3, 9, 10, 13]):
+            print(f"Port {port}: Filtered")
+
+def perform_syn_scan(target, ports):
+    print(f"[~]Scanning target :{target}")
     open_ports = []
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        future_to_ip = {executor.submit(syn_scan, ip, port): ip for ip in ip_addresses}
-        for future in concurrent.futures.as_completed(future_to_ip):
-            ip = future_to_ip[future]
-            result = future.result()
-            open_ports.append(result)
+
+    for port in ports:
+        result = syn_scan(target, port)
+        open_ports.append({port: result})
+
     return open_ports
+
+
+
+
 
 def vanilla_scan_single_host(ip_address, port):
     try:
@@ -66,55 +93,48 @@ def vanilla_scan_single_host(ip_address, port):
     return None
 
 def vanilla_scan(ip_address, ports):
-    open_ports = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = list(executor.map(lambda port: vanilla_scan_single_host(ip_address, port), ports))
-        open_ports.extend([port for port in results if port is not None])
-    return open_ports
+        for port in results:
+            if port is not None:
+                print(f"IP: {ip_address}, Port: {port}, Status: Open")
+
+
+
+
+
 
 def xmas_scan_single_host(ip_address, port):
-    response = sr1(IP(dst=ip_address) / TCP(dport=port, flags="FPU"), timeout=1, verbose=False)
-    if response and response.haslayer(TCP):
-        if response[TCP].flags == 0x14:  # RST packet (port closed)
-            return None
-    return port
+    packet = IP(dst=ip_address) / TCP(dport=port, flags="FPU")
+    response = sr1(packet, timeout=1, verbose=False)
+
+    if response is None:
+        print(f"Port {port}: Open or Filtered")
+        return
+
+    if response.haslayer(TCP):
+        if response[TCP].flags == 0x14:  # RST packet
+            print(f"Port {port}: Closed")
+            return
+
+    if response.haslayer(ICMP):
+        if response[ICMP].type == 3 and response[ICMP].code in [1, 2, 3, 9, 10, 13]:
+            print(f"Port {port}: Filtered")
+            return
+
+    print(f"Port {port}: Unknown")
+
+
 
 def xmas_scan(ip_address, ports):
-    open_ports = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
         results = list(executor.map(lambda port: xmas_scan_single_host(ip_address, port), ports))
-        open_ports.extend([port for port in results if port is not None])
-    return open_ports
+        for port in results:
+            if port is not None:
+                print(f"IP: {ip_address}, Port: {port}, Status: Open/Filtered")
 
 if __name__ == "__main__":
     # ... (your existing code for the main function)
-    COMMON_PORTS = [21, 22, 23, 25, 53, 80, 110, 443, 445, 3389]
-    network_prefix = input("Enter the network prefix (e.g., 192.168.1.0): ")
-    subnet_mask = input("Enter the subnet mask (e.g., 24): ")
-
-    hosts, active_hosts = discover_hosts(network_prefix, subnet_mask)
-    if isinstance(hosts, list):
-         print("Discovered host ip addresses:")
-    for host in hosts:
-        print(f"IP: {host['ip']}, MAC: {host['mac']}")
-        
-        
-    else:
-            print(hosts)    
-
-    
-    syn_open_ports = perform_syn_scan(active_hosts, COMMON_PORTS)
-    vanilla_open_ports = [vanilla_scan(ip, COMMON_PORTS) for ip in active_hosts]
-    xmas_open_ports = [xmas_scan(ip, COMMON_PORTS) for ip in active_hosts]
-
-    print("\nOpen ports found by SYN scan:")
-    for ip, ports in zip(active_hosts, syn_open_ports):
-        print(f"Host: {ip}, Open Ports: {ports}")
-
-    print("\nOpen ports found by Vanilla scan:")
-    for ip, ports in zip(active_hosts, vanilla_open_ports):
-        print(f"Host: {ip}, Open Ports: {ports}")
-
-    print("\nOpen ports found by XMAS scan:")
-    for ip, ports in zip(active_hosts, xmas_open_ports):
-        print(f"Host: {ip}, Open Ports: {ports}")
+ #vanilla_scan("192.168.100.1",(21,22,23,45,53,443,445,139))
+    perform_syn_scan("192.168.100.89",(22,23,25,139,135,109,110,119))
+ 
